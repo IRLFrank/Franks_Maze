e664121ed7e48953eeaf3c0adb5890907ed89368
František Ficker <fickerf24@spstrutnov.cz>
2025-02-06 21:09:16 +0100

    Kolize + bludiste
    Dokončení kolizí, to byla hlavni myšlenka dneška. Čirou náhodou jsem i dodělal testovací lvls a do příště bych jich udělal 10, jak bylo plánováno.

--- Frank's maze.py
+++ Frank's maze.py
@@ -1,13 +1,17 @@
 import pygame
 from pygame.locals import * 
+
 pygame.init()
 
+# Rozměry hlavní obrazovky
 Hlavni_screen_X = 1980  
 Hlavni_screen_Y = 1080
 
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
+# Barvy
+BLACK = (0, 0, 0)  # Barva stěn
+GREEN = (0, 255, 0)  # Barva pozadí
 
+# Načtení obrázků
 Background_menu = pygame.image.load('background.jpg')
 Obraz = pygame.display.set_mode((Hlavni_screen_X, Hlavni_screen_Y))
 
@@ -20,31 +24,39 @@ quit_button_rect = quit_button.get_rect()
 start_button_rect.topleft = ((Hlavni_screen_X - start_button_rect.width) // 2, (Hlavni_screen_Y // 2) - 200)  
 quit_button_rect.topleft = ((Hlavni_screen_X - quit_button_rect.width) // 2, (Hlavni_screen_Y // 2) + 300)  
 
+# Herní okno
 velikost_policka = 40
-
 Herni_okno_X = 800
 Herni_okno_Y = 600
 Herni_okno = pygame.Surface((Herni_okno_X, Herni_okno_Y))
 
-
+# Ukázkové úrovně bludiště (1 = zeď, 0 = volné pole)
 maze_lvl_1 = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1],
-    [1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1],
-    [1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1],
-    [1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,1,1,1,1,1,1,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,0,0,0,0,0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,1,1,1,1,0,1,0,1,0,1,0,1],
-    [1,0,0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+maze_lvl_2 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# Seznam úrovní
+maze_lvls = [maze_lvl_1, maze_lvl_2]
+aktualni_lvl = 0
+maze = maze_lvls[aktualni_lvl]
+
+# Herní proměnné
 main_screen = True
 game_screen = False
 
-Hrac_velikost = 40
+Hrac_velikost = 25
 Hrac_X = 40
 Hrac_Y = 40
 Hrac_speed = 2
@@ -52,6 +64,44 @@ Hrac_speed = 2
 Hrac_textura = pygame.image.load('hrac.png')
 Hrac_textura = pygame.transform.scale(Hrac_textura, (Hrac_velikost, Hrac_velikost))
 
+def check_level_complete():
+    """Kontrola, zda hráč dokončil úroveň"""
+    global aktualni_lvl, maze, Hrac_X, Hrac_Y, main_screen, game_screen
+    
+    misto_x = (len(maze[0]) - 2) * velikost_policka
+    misto_y = (len(maze) - 2) * velikost_policka
+
+    if Hrac_X >= misto_x - 10 and Hrac_Y >= misto_y - 10:
+        if aktualni_lvl + 1 < len(maze_lvls):  
+            aktualni_lvl += 1
+            maze = maze_lvls[aktualni_lvl]
+            Hrac_X, Hrac_Y = 40, 40  # Reset pozice hráče
+        else:
+            # Po dokončení všech úrovní se vrátí do menu
+            main_screen = True
+            game_screen = False
+            aktualni_lvl = 0
+            maze = maze_lvls[aktualni_lvl]
+            Hrac_X, Hrac_Y = 40, 40
+
+def byla_kolize(nove_x, nove_y):
+    """Zkontroluje, zda hráč narazí do zdi"""
+    body_ke_kontrole  = [         
+        (nove_x, nove_y),  
+        (nove_x + Hrac_velikost - 1, nove_y),  
+        (nove_x, nove_y + Hrac_velikost - 1),  
+        (nove_x + Hrac_velikost - 1, nove_y + Hrac_velikost - 1)  
+    ]
+
+    for bx, by in body_ke_kontrole:
+        maze_x = bx // velikost_policka
+        maze_y = by // velikost_policka
+        if maze[maze_y][maze_x] == 1:  
+            return True  
+    
+    return False  
+
+# Hlavní smyčka
 smycka = True
 while smycka:
     for event in pygame.event.get():
@@ -74,36 +124,34 @@ while smycka:
     if game_screen:
         Herni_okno.fill(GREEN)  
 
-        
-        for radky in range(len(maze_lvl_1)):
-            for sloupce in range(len(maze_lvl_1[radky])):
-                if maze_lvl_1[radky][sloupce] == 1:
+        # Vykreslení bludiště
+        for radky in range(len(maze)):
+            for sloupce in range(len(maze[radky])):
+                if maze[radky][sloupce] == 1:
                     pygame.draw.rect(Herni_okno, BLACK, (sloupce * velikost_policka, radky * velikost_policka, velikost_policka, velikost_policka))
 
-        
+        # Pohyb hráče
         keys = pygame.key.get_pressed()
-        if keys[K_w]:
-            Hrac_Y -= Hrac_speed
-        if keys[K_s]:
-            Hrac_Y += Hrac_speed
-        if keys[K_a]:
-            Hrac_X -= Hrac_speed
-        if keys[K_d]:
-            Hrac_X += Hrac_speed
-        
-        
-        Hrac_X = max(0, min(Herni_okno_X - Hrac_velikost, Hrac_X))
-        Hrac_Y = max(0, min(Herni_okno_Y - Hrac_velikost, Hrac_Y))
+        new_x, new_y = Hrac_X, Hrac_Y  
 
-        
+        if keys[K_w]:
+            new_y -= Hrac_speed
+        if keys[K_s]:
+            new_y += Hrac_speed
+        if keys[K_a]:
+            new_x -= Hrac_speed
+        if keys[K_d]:
+            new_x += Hrac_speed
+
+        if not byla_kolize(new_x, new_y):  
+            Hrac_X, Hrac_Y = new_x, new_y  
+
         Herni_okno.blit(Hrac_textura, (Hrac_X, Hrac_Y))
 
-        Herni_okno_text = pygame.font.SysFont('Arial', 50).render('Herní obrazovka', True, (255, 255, 255))
-        Herni_okno.blit(Herni_okno_text, (Herni_okno_X // 2 - Herni_okno_text.get_width() // 2, 50))
+        check_level_complete()
 
-        
         Obraz.blit(Herni_okno, (Hlavni_screen_X // 2 - Herni_okno_X // 2, Hlavni_screen_Y // 2 - Herni_okno_Y // 2))
-
+        
     pygame.display.update()
 
 pygame.quit()
